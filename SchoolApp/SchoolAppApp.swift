4441c763d904c6144b61382c411aa29f262d550f
Cody Heaven <heavenerc@baldwynschools.com>
2022-03-10 08:20:28 -0600

    Initial Commit

---
+++ SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by Cody Heavener on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
